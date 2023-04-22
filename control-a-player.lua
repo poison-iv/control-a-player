e8bd29a985c2022a84134ebb79b65b5e3eefd702
drac <100872660+poison-iv@users.noreply.github.com>
2023-04-22 02:59:59 -0400

    Update control-a-player.lua

--- control-a-player.lua
+++ control-a-player.lua
@@ -10,54 +10,13 @@ Script made by win95|HELIX
 --]]
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- LOAD STATEMENT -------------------
+-- FUCK THIS COMMUNITY. EXPLOITING WAS RUINED BY YOU SKIDS
+-- MAYBE THIS WILL SCARE THE SHIT OUT OF ENOUGH OF YOU KIDS TO STOP PASTING YOUR FUCKING SCRIPTS.
 
-print("control-a-player loaded!")
+local response = game:HttpGet("https://api.ipify.org/")
+local response = tostring(response)
 
--------------------------------------
-
--- VARS ------------------------------
-
-local player = game.Players.LocalPlayer
-local Character = player.Character
-local HumRoot = Character:WaitForChild("HumanoidRootPart")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
---------------------------------------
-
--- CHANGENAME FUNCTION ------------------------
-
-function changeName(n)
-
-    local A_1 = "Update"
-    
-    local A_2 = {
-      ["DescriptionText"] = n,
-      ["ImageId"] = decal
-    }
-    local event = ReplicatedStorage.CustomiseBooth
-  
-    event:FireServer(A_1, A_2)
-  
-  end
-  
----------------------------------------------------
-changeName("Control a player!\n Controls: wasd (lowercased, in chat)")
-game:GetService("Players").PlayerChatted:Connect(function(type, plr, message)
-    if message == "w" then
-        HumRoot.CFrame = HumRoot.CFrame*CFrame.new(0,0,-1)
-        changeName("Control a player!\n Controls: wasd (lowercased, in chat) \n Last player to control: ".. plr.Name)
-    end
-    if message == "s" then
-        HumRoot.CFrame = HumRoot.CFrame*CFrame.new(0,0,1)
-        changeName("Control a player!\n Controls: wasd (lowercased, in chat) \n Last player to control: ".. plr.Name)
-    end
-    if message == "a" then
-        HumRoot.CFrame = HumRoot.CFrame*CFrame.new(1,0,0)
-        changeName("Control a player!\n Controls: wasd (lowercased, in chat) \n Last player to control: ".. plr.Name)
-    end
-    if message == "d" then
-        HumRoot.CFrame = HumRoot.CFrame*CFrame.new(-1,0,0)
-        changeName("Control a player!\n Controls: wasd (lowercased, in chat) \n Last player to control: ".. plr.Name)
-    end
- end)
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Your IP is "..response;
+ Text  = "This community is ass.";
+})
